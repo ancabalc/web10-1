3607f5627d7d2bda63613849d1f0bc7025b3e1ae
Andrea Marginean <andreaheltai@gmail.com>
2017-07-12 05:33:24 +0000

    Arrange Accounts.php to be easier to read

--- api/controllers/Accounts.php
+++ api/controllers/Accounts.php
@@ -13,10 +13,12 @@ class Accounts {
         if (empty($_POST['email']) || empty($_POST['password'])) {
             return 'Invalid fields';
         } else {
+            
             $salt = '$1$12!ab';
             $_POST['password'] = crypt($_POST["password"], $salt);
             
             $result = $this->usersModel->checkLogin($_POST);
+            
             if (!empty ($result)) {
                 $_SESSION["isLogged"] = TRUE;
                 $_SESSION["email"] = $_POST['email'];
