aadf596889e2a9113524d7142ea7fb54a019f492
julean vlad <julean.vlad@yahoo.com>
2017-07-18 16:02:23 +0000

    email api

--- api/models/UsersModels.php
+++ api/models/UsersModels.php
@@ -2,7 +2,7 @@
 require_once "DB.php";
     class UsersModel extends DB {
         function getTop3() {
-            $query = "SELECT name,description,image FROM users ORDER BY id limit 3";
+            $query = "SELECT name,description,image FROM users ORDER BY id desc limit 3";
             return $this->executeQuery($query);
         }
     }
