de6a4333dbec868e67cd17525537f5d5020e8b2a
Dobri Attila <dati_nyul@yahoo.com>
2017-07-25 17:08:05 +0000

    update

--- ui/index.php
+++ ui/index.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <div class='offers-list'>
-    </div>
+     <?php include ("layout/navbar.php"); ?>
+    <div class='offers-list'></div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/main.js"></script>
-<script>
-
-</script>
-</body>
+    <script type="text/javascript" src=""></script>
+    <?php include ("layout/footer.php"); ?>
+ </body>    
 </html>
+    
+
